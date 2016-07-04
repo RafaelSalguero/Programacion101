2d9e939ec561b04fcfcb134df1262ed8c3b8149a
Gabriel Alfonso Ramos Pitones <gabriel.ramos.0812@gmail.com>
2016-07-04 12:47:06 -0700

    examen if

--- Parte 2 - Control de flujo/02 Prueba.cs
+++ Parte 2 - Control de flujo/02 Prueba.cs
@@ -7,6 +7,21 @@ que devuelva el area de un rectangulo si tipo == "rectangulo"}
 
 Si tipo no es ni "rectangulo" ni "triangulo", debe de devolver -1 
 **/
-static int area(string tipo, int b, int a) {
+static int area(string tipo, int b, int a) 
+{
 
+    if (tipo = "triangulo")
+        {
+            area = (b * a)/2;
+        }
+    else if (tipo = "rectagulo")
+        {
+            area = b * a;
+        }
+        else
+        {
+            area = -1;
+        }
+
+    return area;
 }
