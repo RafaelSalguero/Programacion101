f016590d3b60eec7bdcdbdc7b404542e05434551
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-07-04 13:04:55 -0700

    arreglos

---
+++ Parte 3 - Arreglos/01 - arreglos.cs
@@ -0,0 +1,27 @@
+//************************************************
+//Arreglos
+//************************************************
+
+/*
+Un arreglo es un tipo especial, el cual sirve para almacenar una *coleccion ordenada* de valores de cierto tipo
+
+por ejemplo el tipo:
+int[]
+
+es un arreglo de numeros enterios
+
+La variable:
+
+int[] edades;
+
+Puede almacenar no una, si no cero, una o muchas edades
+
+Cada dato en el arreglo se conoce como *elemento*
+Cualquier tipo puede ser el tipo de elemento de un arreglo, incluso otros arreglos:
+
+Esta variable es un arreglo de un arreglo de un arreglo de numeros enteros
+int[][][] variable;
+
+
+
+*/
