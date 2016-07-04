8487a8cbdeaf9eb86382a95bf456da63bee82e7f
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-07-04 16:37:58 -0700

    prueba

---
+++ Parte 3 - Arreglos/03 - prueba.cs
@@ -0,0 +1,14 @@
+//Escriba una funcion que devuelva la suma de todos los elementos de un arreglo
+int sumatoria(int[] valores) {
+
+}
+
+//Escriba una función que devuelva el valor del elemento mas grande de un arreglo
+int mayor (int[] valores) {
+
+}
+
+//Escriba una funcion que cuente que tantos elementos de un arreglo son iguales a "igualA"
+int contar(int[] valores, int igualA) {
+
+}
