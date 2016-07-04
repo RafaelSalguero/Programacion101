b7d8050f63e85199eb4fb1e59adafde384de7efc
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-07-04 13:25:51 -0700

    recorrer un arreglo

---
+++ Parte 3 - Arreglos/02 - recorrer un arreglo.cs
@@ -0,0 +1,16 @@
+//***************************************
+//Recorrer un arreglo
+//***************************************
+
+/*
+Recorrer un arreglo implica ejecutar un bloque de codigo para *cada uno de sus elementos* independientemente de la cantidad
+de elementos que existan en el mismo.
+
+Esta es una de las operaciones mas comunes en la programación ya que la gran mayoria de los datos en los programas estan almacenados
+en arreglos.
+*/
+
+//Utilizando el ciclo while
+
+//Recordando al ciclo while, que ejecuta un bloque siempre que se cumpla una condicion, 
+//la logica aqui es incrementar una variable hasta que sea 
