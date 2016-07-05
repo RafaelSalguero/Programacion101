d1c2a7442d22f119c48365d904ccba85fc082b04
Gabriel Alfonso Ramos Pitones <gabriel.ramos.0812@gmail.com>
2016-07-04 17:07:34 -0700

    examen arrays

--- Parte 3 - Arreglos/03 - prueba.cs
+++ Parte 3 - Arreglos/03 - prueba.cs
@@ -1,14 +1,43 @@
 //Escriba una funcion que devuelva la suma de todos los elementos de un arreglo
 int sumatoria(int[] valores) {
+    int sum;
+    for (i=0 ; i > valores.Lenght ; i ++ )
+    {
+        sum = valores[i];
+    }
+     System.Console.WriteLine("La suma es: " + sum);
 
 }
 
 //Escriba una función que devuelva el valor del elemento mas grande de un arreglo
 int mayor (int[] valores) {
+    int mayor=0;
+    for (i=0; i > valores.lenght ; i++)
+    {
+        if (mayor < valores [i])
+        {
+            mayor = valores [i];
+        }
+        
+    }
 
 }
 
 //Escriba una funcion que cuente que tantos elementos de un arreglo son iguales a "igualA"
 int contar(int[] valores, int igualA) {
+
+    int contador;
+    for (i=0; i > valores.lenght ; i++)
+    {
+        if (valores[i] == "igualA")
+        {
+            contador = contador + 1;
+        }
+        
+    }
+
+    console.writeline ("el numero de repeticiones de igualA es: "+contador)
+    
+
 
 }
